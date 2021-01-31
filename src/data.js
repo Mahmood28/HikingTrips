afc930ef2600c775bc7071fa10b447bb1ceef227
Mahmood28 <mahmoodalwatani@gmail.com>
2021-01-31 09:45:45 +0300

    First commit.

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    name: "Walking in Madrid",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.nakedmadrid.com%2Fwp-content%2Fuploads%2F2014%2F11%2FHiking-Madrid.jpg",
+    city: "Madrid",
+    difficulty: "medium",
+    length: 5,
+    rating: 2,
+  },
+  {
+    name: "Walking in Paris",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.nakedmadrid.com%2Fwp-content%2Fuploads%2F2014%2F11%2FHiking-Madrid.jpg",
+    city: "Paris",
+    difficulty: "hard",
+    length: 12,
+    rating: 4,
+  },
+  {
+    name: "Walking in Bahrain",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.nakedmadrid.com%2Fwp-content%2Fuploads%2F2014%2F11%2FHiking-Madrid.jpg",
+    city: "Manama",
+    difficulty: "easy",
+    length: 20,
+    rating: 3,
+  },
+];
